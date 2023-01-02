7aaa4d1cd3c8977e5ff0b9f5bcff6a8e44c04f46
greatbot6120 <74972911+greatbot6120@users.noreply.github.com>
2023-01-02 06:31:04 +0100

    Add files via upload

---
+++ Sorgente/Esercitazioni/sorgente_laboratorio/newMatrix.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+#define DIM_ONE 5
+#define DIM_TWO 2
+
+int main(int argc, char* argv[]) {
+
+    int firstArr[DIM_ONE][DIM_ONE] = {{3, 5, 54, 93, 23}, {4, 9, 65, 89, 77}, {3, 5, 912, 81, 97}, {4, 9, 25, 78, 79}, {932, 452, 4398, 876, 5433}};
+    int secondArr[DIM_TWO][DIM_TWO] = { {3, 5}, {4, 9} };
+    int indexLoop, subIndexLoop, matchStatus, equalityCounter = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*printf("**FIRST MATRIX**\n");
+    
+    for(indexLoop = 0; indexLoop < DIM_ONE; indexLoop++) {
+
+        for(subIndexLoop = 0; subIndexLoop < DIM_ONE; subIndexLoop++) {
+
+            printf("Enter element [%d][%d] => ", indexLoop, subIndexLoop);
+            scanf("%d", &firstArr[indexLoop][subIndexLoop]);
+        }
+    }
+
+    printf("**SECOND MATRIX**\n");
+
+    for(indexLoop = 0; indexLoop < DIM_TWO; indexLoop++) {
+
+        for(subIndexLoop = 0; subIndexLoop < DIM_TWO; subIndexLoop++) {
+
+            printf("Enter element [%d][%d] => ", indexLoop, subIndexLoop);
+            scanf("%d", &secondArr[indexLoop][subIndexLoop]);
+        }
+    }*/
